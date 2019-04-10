6d5f68150b5aa47e4eda9a72abf651e3da8f844d
Bastien Tran <bastien.tran@gmail.com>
2019-04-11 00:00:44 +0200

    sign up & log in appear to be working

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const logger       = require('morgan');
 const path         = require('path');
 const passport     = require("passport");
 const session = require("express-session");
+const flash = require("connect-flash");
 const MongoStore = require("connect-mongo")(session);
 
 // run the code inside the "passport-setup.js"
@@ -58,6 +59,24 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// allow our routes to use FLASH MESSAGES -- feedback messages before redirects
+// (flash messages need sessions to work)
+app.use(flash());
+//app.use() defines our own MIDDLEWARE function
+app.use((req, res, next) => {
+  // send flash messages to the hbs file
+  // (req.flash() comes from the "connect-flash" npm package)
+  res.locals.messages = req.flash();
+  // send the logged-in user's info to ALL hbs files
+  // (req.user is defined by Passport and contains the logged-in user's info)
+
+  res.locals.currentUser = req.user;
+
+  // tell Express we are ready to move to the routes now
+  // (you need this or your pages will stay loading forever)
+  next();
+});
 
 app.locals.title = 'Quizz Generator';
 
