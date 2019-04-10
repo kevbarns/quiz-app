9b6893a83271f4cdc0221e107327f20f4554d988
Bastien Tran <bastien.tran@gmail.com>
2019-04-10 17:54:26 +0200

    dashboard-route.js now has routers for creating a quiz, getting the quizzes created by the user and getting one single quizz

--- routes/dashboard-route.js
+++ routes/dashboard-route.js
@@ -2,17 +2,38 @@ const express = require('express');
 const router  = express.Router();
 
 const Quiz = require("../models/quiz-model.js");
-const QuizDetail = require("../models/quiz-detail-model.js");
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  // req.user comes from Passport's deserializeUser()
-  if (req.user) {
-    console.log("WE ARE LOGGED IN!", req.user);
-  } else {
-    console.log("NOT LOGGED IN!", req.user);
-  }
-  res.render("index");
+// Get our user's quizzes
+router.get("/quizzes", (req, res, next) => {
+  const currentUser = req.user._id;
+  // whenever a user visits "/quizzes" find all the quizzes they have created so far
+  Book.find({ userId: { $eq: currentUser }})
+    .sort({ timestamp: -1 }) // how do we want them sorted ?
+    .then(quizResults => {
+      // send the database query results to the HBS file as "quizArray"
+      res.locals.quizArray = quizResults;
+      res.render("quiz-list.hbs"); // Maybe the corresponding view is named or to be named otherwise
+    })
+    // next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
+});
+
+
+// Get one quiz
+// ##################################################
+// Maybe we want to edit the quiz details from here ?
+// ##################################################
+router.get("/quiz/:quizId", (req, res, next) => {
+  const { quizId } = req.params;
+  // find the quiz in the database using the ID passed as a path parameter
+  Book.findById(quizId)
+    .then(quizDoc => {
+      // send the query results to the HBS file as "quizItem"
+      res.locals.quizItem = quizDoc;
+      res.render("quiz-details.hbs");
+    })
+    //
+    .catch(err => next(err));
 });
 
 
