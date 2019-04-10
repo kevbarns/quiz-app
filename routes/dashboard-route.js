a13a8be145a19a65147a5d1fefd035e38ef5219c
Bastien Tran <bastien.tran@gmail.com>
2019-04-10 17:25:27 +0200

    added the post route for creating a new quiz

--- routes/dashboard-route.js
+++ routes/dashboard-route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 
+const Quiz = require("../models/quiz-model.js");
+const QuizDetail = require("../models/quiz-detail-model.js");
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   // req.user comes from Passport's deserializeUser()
@@ -11,5 +14,21 @@ router.get('/', (req, res, next) => {
   }
   res.render("index");
 });
+
+
+// Create a new quizz
+router.post("/process-quiz", (req, res, next) => {
+  const { name, description, theme, url } = req.body;
+  // res.json(req.body);
+  Quiz.create({ name, description, theme, url })
+    .then(quizDoc => {
+      // redirect if it's successful
+      // (redirect ONLY to addresses - not to HBS files)
+      res.redirect(`/quiz/${quizDoc._id}`);
+    })
+    // next(err) skips to the rerror handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
+});
+
 
 module.exports = router;
