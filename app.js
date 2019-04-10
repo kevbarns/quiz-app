847573261da25a7cc5ee3402828c52edf8a84e90
Kevin <kevin@cestfrais.fr>
2019-04-10 16:36:40 +0200

    Global update to correct app crash

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 const passport     = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 // run the code inside the "passport-setup.js"
 require("./config/passport-setup.js");
@@ -34,54 +36,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-// make our Express app create SESSIONS (more on this tomorrow)
 app.use(
   session({
-    // saveUninitialized & resave are just to avoid warning messages
     saveUninitialized: true,
     resave: true,
-    // should be a string that's different for every app
     secret: "ca^khT8KYd,G69C7R9(;^atb?h>FTW6664pqEFUKs3",
-    // store our session data inside our MongoDB using "connect-mongo" package
     store: new MongoStore({ mongooseConnection: mongoose.connection })
   })
 );
 
-// set up Passport's methods to use in our routes
 app.use(passport.initialize());
-// make passport manage our user session
 app.use(passport.session());
 
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
+app.locals.title = 'Quizz Generator';
 
 const index = require('./routes/index.js');
 app.use('/', index);
 
-const athentification = require("./routes/athentification-route.js");
-app.use("/", auth);
+const authentification = require("./routes/authentification-route.js");
+app.use("/", authentification);
 
 const dashboard = require("./routes/dashboard-route.js");
-app.use("/", room);
+app.use("/", dashboard);
 
 const frontoffice = require("./routes/frontoffice-route.js");
-app.use("/", room);
+app.use("/", frontoffice);
 
 module.exports = app;
---
+++ routes/authentification-route.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user-model.js");
+
+const router = express.Router();
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth-views/signup-form.hbs");
+});
+
+router.post("/process-signup", (req, res, next) => {
+  const { fullName, email, originalPassword } = req.body;
+
+  // enforce password rules (can't be empty and MUST have a digit)
+  if (!originalPassword || !originalPassword.match(/[0-9]/)) {
+    req.flash("error", "Password can't be blank and must contain a number.");
+    res.redirect("/signup");
+    return;
+  }
+
+  // encrypt the user's password before saving it
+  const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
+
+  User.create({ fullName, email, encryptedPassword })
+    .then(userDoc => {
+      sendSignupMail(userDoc)
+        .then(() => {
+          req.flash("success", "Sign up success! 😃");
+          res.redirect("/");
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth-views/login-form.hbs");
+});
+
+router.post("/process-login", (req, res, next) => {
+  const { email, originalPassword } = req.body;
+  User.findOne({ email: { $eq: email } })
+    .then(userDoc => {
+      if (!userDoc) {
+        req.flash("error", "Email is incorrect.");
+        res.redirect("/login");
+        return;
+      }
+
+      const { encryptedPassword } = userDoc;
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        req.flash("error", "Password is incorrect.");
+        res.redirect("/login");
+        return;
+      }
+      req.logIn(userDoc, () => {
+        req.flash("success", "Log in success!");
+        res.redirect("/");
+      });
+    })
+    .catch(err => next(err));
+});
+
+router.get("/logout", (req, res, next) => {
+  req.logOut();
+  req.flash("success", "Logged out successfully!");
+  res.redirect("/");
+});
+
+module.exports = router;
