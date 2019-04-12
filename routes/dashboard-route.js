a3d8f190f0a8ab78a1bec68047aa62277ebb46fd
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-04-12 16:49:54 +0200

    quiz card styling

--- routes/dashboard-route.js
+++ routes/dashboard-route.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const Quiz = require("../models/quiz-model.js");
 const QuizDetail = require("../models/quiz-detail-model.js");
@@ -14,34 +14,28 @@ router.use("/", (req, res, next) => {
   next();
 });
 
-router.get("/dashboard", (req,res,next) => {
-  res.render("dashboard/dashboard-home");
-})
-
 // route dashboard fin de creation du quiz
 // TODO : Proposer un bouton pour y accéder dans un nouvelle onglet et afficher l'URL
 
 // route dashboard new quiz
 router.get("/new", (req, res, next) => {
-  console.log("COUCOU");
-  res.render("./quiz-form.hbs");
+  res.render("dashboard/quiz-form.hbs");
 });
 
 // Get our user's quizzes
-router.get("/quizzes", (req, res, next) => {
+router.get("/dashboard", (req, res, next) => {
   const currentUser = req.user._id;
   // whenever a user visits "/quizzes" find all the quizzes they have created so far
-  Quiz.find({ userId: { $eq: currentUser }})
+  Quiz.find({ userId: { $eq: currentUser } })
     .sort({ timestamp: -1 }) // how do we want them sorted ?
     .then(quizResults => {
       // send the database query results to the HBS file as "quizArray"
       res.locals.quizArray = quizResults;
-      res.render("quiz-list.hbs"); // Maybe the corresponding view is named or to be named otherwise
+      res.render("dashboard/dashboard-home.hbs"); // Maybe the corresponding view is named or to be named otherwise
     })
     // next(err) skips to the error handler in "bin/www" (error.hbs)
     .catch(err => next(err));
 });
-
 
 // Get one quiz
 // ##################################################
@@ -54,15 +48,14 @@ router.get("/quiz/:quizId", (req, res, next) => {
     .then(quizDoc => {
       // send the query results to the HBS file as "quizItem"
       res.locals.quizItem = quizDoc;
-      QuizDetail.find({quizId: { $eq: quizId}})
-              .then(quizDetailArray => {
-                // send the query results to the HBS file as "quizItem"
-                res.locals.cardList = quizDetailArray;
-                res.render("quiz-views/quiz-details.hbs");
-
-              })
-              //
-              .catch(err => next(err));
+      QuizDetail.find({ quizId: { $eq: quizId } })
+        .then(quizDetailArray => {
+          // send the query results to the HBS file as "quizItem"
+          res.locals.cardList = quizDetailArray;
+          res.render("dashboard/quiz-details.hbs");
+        })
+        //
+        .catch(err => next(err));
     })
     //
     .catch(err => next(err));
@@ -75,7 +68,7 @@ router.post("/process-quiz", (req, res, next) => {
   const userId = req.user._id;
   const { name, description, theme, url } = req.body;
   // res.json(req.body);
-  Quiz.create({ userId,  name, description, theme, url })
+  Quiz.create({ userId, name, description, theme, url })
     .then(quizDoc => {
       // redirect if it's successful
       // (redirect ONLY to addresses - not to HBS files)
@@ -86,42 +79,44 @@ router.post("/process-quiz", (req, res, next) => {
     .catch(err => next(err));
 });
 
-
 // Create a new card (should return a card array)
 router.post("/quiz/:quizId/process-card", (req, res, next) => {
   // req.user comes from Passport's deserializeUser()
   // (it's the document from the database of the logged-in user)
   const { quizId } = req.params;
   // Maybe we don't get the answers and thruth as arrays and we will need to build it here
-  const { question, 
-          firstAnswer,
-          secondAnswer,
-          thirdAnswer,
-          fourthAnswer,
-          firstTrue,
-          secondTrue,
-          thirdTrue,
-          fourthTrue 
-        } = req.body; 
+  const {
+    question,
+    firstAnswer,
+    secondAnswer,
+    thirdAnswer,
+    fourthAnswer,
+    firstTrue,
+    secondTrue,
+    thirdTrue,
+    fourthTrue
+  } = req.body;
   // res.json(req.body);
-  QuizDetail.create({ quizId, 
-                      question, 
-                      options: [
-                        { option: firstAnswer, valid : firstTrue },
-                        { option: secondAnswer, valid : secondTrue },
-                        { option: thirdAnswer, valid : thirdTrue },
-                        { option: fourthAnswer, valid : fourthTrue }
-                      ]})
+  QuizDetail.create({
+    quizId,
+    question,
+    options: [
+      { option: firstAnswer, valid: firstTrue },
+      { option: secondAnswer, valid: secondTrue },
+      { option: thirdAnswer, valid: thirdTrue },
+      { option: fourthAnswer, valid: fourthTrue }
+    ]
+  })
     .then(() => {
-      QuizDetail.find({quizId: { $eq: quizId}})
-              .sort({timestamp: -1})
-              .then(quizDetailArray => {
-                // send the query results to the HBS file as "quizItem"
-                res.locals.cardList = quizDetailArray;
-                res.redirect(`/quiz/${quizId}`);
-              })
-              //
-              .catch(err => next(err));
+      QuizDetail.find({ quizId: { $eq: quizId } })
+        .sort({ timestamp: -1 })
+        .then(quizDetailArray => {
+          // send the query results to the HBS file as "quizItem"
+          res.locals.cardList = quizDetailArray;
+          res.redirect(`/quiz/${quizId}`);
+        })
+        //
+        .catch(err => next(err));
       // redirect if it's successful
       // (redirect ONLY to addresses - not to HBS files)
     })
